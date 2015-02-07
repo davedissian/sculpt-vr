8c1e2f652a55eee17289ecea82ea5dfdae9b072e
David Avedissian <avedissian.david@gmail.com>
2015-02-07 14:34:24 +0000

    Added some test code and YouCompleteMe config

--- main.cc
+++ main.cc
@@ -1,6 +1,20 @@
+#include <iostream>
+using namespace std;
 
+#include <OVR.h>
 
 int main()
 {
+  ovr_Initialize();
+  ovrHmd hmd = ovrHmd_Create(0);
+  if (!hmd)
+  {
+    cerr << "There was an error creating the HMD handle - is the rift plugged in?" << endl;
+    return 100;
+  }
+
+  // Get more details
+  cout << "Res X: " << hmd->Resolution.w << " - Res Y: " << hmd->Resolution.h << endl;
+  
   return 0;
 }
